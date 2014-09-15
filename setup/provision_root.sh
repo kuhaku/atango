9ab09cde5793bcf0a46c3b04d12974ddcb59ca3a
kuhaku <kuhaku.mami@gmail.com>
2014-09-15 21:13:23 +0900

    Add Japan timezone setting to vagrant provision

--- setup/provision_root.sh
+++ setup/provision_root.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 apt-get update
+
 apt-get install -y build-essential python-dev python-pip
 apt-get install -y gfortran liblapack-dev libblas-dev
 apt-get install -y libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
 
 wget --no-check-certificate https://raw.githubusercontent.com/kuhaku/atango/master/requirements.txt
 pip install --upgrade -r requirements.txt
+rm requirements.txt
+
 apt-get install -y mecab-ipadic-utf8 python-mecab
 apt-get install -y mecab
 
@@ -13,4 +16,4 @@ apt-get install -y git
 mkdir -p /work
 chmod 777 /work
 
-rm requirements.txt
+ln -sf /usr/share/zoneinfo/Japan /etc/localtime
