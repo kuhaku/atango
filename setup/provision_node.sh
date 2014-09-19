ff6ce3cbfe6faddee109604f54965bf5aad296ef
kuhaku <kuhaku.mami@gmail.com>
2014-09-20 01:36:11 +0900

    :apple: :penguin: Use virtualenv in provisioning

--- setup/provision_node.sh
+++ setup/provision_node.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
-git clone https://github.com/kuhaku/atango.git /work/atango
-echo export PYTHONPATH=/work/atango/lib >> ~/.bashrc
+#!/bin/bash
 
+# Create virtualenv
+virtualenv /work/venv/atango
+. /work/venv/atango/bin/activate
+
+# Install python libs from PyPI
+wget --no-check-certificate https://raw.githubusercontent.com/kuhaku/atango/master/requirements.txt
+pip install --upgrade -r requirements.txt
+rm requirements.txt
+
+# Install Python-MeCab
+MECAB_VERSION=`mecab-config --version`
+pip install https://mecab.googlecode.com/files/mecab-python-${MECAB_VERSION}.tar.gz
+
+# Clone Atango repository
+git clone https://github.com/kuhaku/atango.git /work/atango
+echo export PYTHONPATH=/work/atango/lib:${PYTHONPATH} >> /work/venv/atango/bin/activate
+
+# Configure matplotlib 
 mkdir -p ~/.config/matplotlib/
 echo backend : agg > ~/.config/matplotlib/matplotlibrc
--- setup/provision_root.sh
+++ setup/provision_root.sh
@@ -1,22 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+
 apt-get update
 
+# Install things for building
 apt-get install -y build-essential python-dev python-pip
+
+# Install virtualenv
+pip install virtualenv
+
+# Install scipy dependencies
 apt-get install -y gfortran liblapack-dev libblas-dev
-apt-get install -y libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
 
-wget --no-check-certificate https://raw.githubusercontent.com/kuhaku/atango/master/requirements.txt
-pip install --upgrade -r requirements.txt
-rm requirements.txt
+# Install matplotlib dependencies
+apt-get install -y libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev
 
-apt-get install -y mecab-ipadic-utf8 python-mecab
-apt-get install -y mecab
+# Install MeCab
+apt-get install -y mecab-ipadic-utf8
+apt-get install -y mecab libmecab-dev
 
+# Install git
 apt-get install -y git
-mkdir -p /work
-chmod 777 /work
 
+# Set Japan timezone
 ln -sf /usr/share/zoneinfo/Japan /etc/localtime
 
 # Install substitute fonts to use in WordMap job
-apt-get install fonts-migmix
+apt-get install -y fonts-migmix
+
+# Create work directory
+mkdir /work
+chmod 777 /work
