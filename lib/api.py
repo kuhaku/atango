bfe802afae96580ce7651ee3c09701ce705c7584
kuhaku <kuhaku.mami@gmail.com>
2014-11-06 22:29:43 +0900

    config.read returns dict

--- lib/api.py
+++ lib/api.py
@@ -19,11 +19,11 @@ TwitterHTTPError.__str__ = __str__patch
 class Twitter:
 
     def __init__(self):
-        config_parser = config.read(CFG_FILE)
-        oauth = twitter.OAuth(config_parser.get('Twitter', 'access_token_key'),
-                              config_parser.get('Twitter', 'access_token_secret'),
-                              config_parser.get('Twitter', 'consumer_key'),
-                              config_parser.get('Twitter', 'consumer_secret'))
+        twitter_config = config.read(CFG_FILE)['Twitter']
+        oauth = twitter.OAuth(twitter_config['access_token_key'],
+                              twitter_config['access_token_secret'],
+                              twitter_config['consumer_key'],
+                              twitter_config['consumer_secret'])
         self.api = twitter.Twitter(auth=oauth)
 
 
@@ -33,7 +33,7 @@ class Flickr:
         config_parser = config.read(CFG_FILE)
         for idx in ('username', 'user_nsid', 'api_key', 'api_secret',
                     'oauth_token_key', 'oauth_token_secret'):
-            setattr(self, idx, config_parser.get('Flickr', idx))
+            setattr(self, idx, config_parser['Flickr'][idx])
 
     def upload(self, filename, title='', description='', tags=''):
         command = ['bash', '/work/atango/bin/flickr.sh']
