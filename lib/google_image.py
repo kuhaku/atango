6d48eaa2291e5c8e20bcb37bffb36c7d343e4171
kuhaku <kuhaku.mami@gmail.com>
2014-12-25 00:16:55 +0900

    Enable image search by word

--- lib/google_image.py
+++ lib/google_image.py
@@ -1,27 +1,31 @@
 # -*- coding: utf-8 -*-
 import re
 from collections import Counter
+from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
 from . import web, file_io
 from .nlp import mecab
 
 
-GOOGLE_BASE_URL = 'http://www.google.co.jp/'
-GOOGLE_SEARCH_BY_ENDPOINT = 'http://images.google.co.jp/searchbyimage?hl=ja&image_url='
-
-re_best_guess = re.compile(u'この画像の最良の推測結果:.*?>(.*?)</a>',
-                           re.IGNORECASE | re.MULTILINE)
+SEARCH_BY_WORD_URL = 'https://www.google.co.jp/search'
+SEARCH_BY_IMAGE_URL = 'http://images.google.co.jp/searchbyimage'
+REFERER = 'http://www.google.co.jp/imghp'
+re_best_guess = re.compile('この画像の最良の推測結果:.*?>(.*?)</a>', re.I | re.M)
 re_html_tag = re.compile('<[^>]+>')
 
 
-class GoogleSearchByImageUtils(object):
+class GoogleImageSearch(object):
     ng_tags = file_io.read('ng_tags.json')
 
     @staticmethod
-    def search(image_url):
-        result_url = GOOGLE_SEARCH_BY_ENDPOINT + image_url
-        referer = 'http://www.google.co.jp/imghp'
-        return web.open_url(result_url, referer=referer)
+    def search_by_url(image_url):
+        params = {'hl': 'ja', 'image_url': image_url}
+        return web.open_url(SEARCH_BY_IMAGE_URL, params=params, referer=REFERER)
+
+    @staticmethod
+    def search_by_word(q, num=50):
+        params = {'q': q, 'num': num, 'safe': 'off', 'tbm': 'isch'}
+        return web.open_url(SEARCH_BY_WORD_URL, params=params, referer=REFERER)
 
     @staticmethod
     def extract_best_guess_tag(html):
@@ -90,19 +94,31 @@ class GoogleSearchByImageUtils(object):
                     most_common_kwds.append(keyword)
         return self.slim_keywords(most_common_kwds)
 
+    def extract_all_image_urls(self, soup):
+        urls = []
+        for a in soup.find_all('a'):
+            query = urlparse(a.get('href')).query
+            for imgurl in parse_qs(query).get('imgurl', []):
+                urls.append(imgurl)
+        return urls
 
-def search(image_url, best_kwds_max_length=50):
-    sbiutil = GoogleSearchByImageUtils()
+def search(q, best_kwds_max_length=50):
+    sbiutil = GoogleImageSearch()
     result = {}
-
-    result['result_page'] = sbiutil.search(image_url)
-    result['best_guess'] = sbiutil.extract_best_guess_tag(result['result_page'])
-    soup_result_page = BeautifulSoup(result['result_page'])
-    if not sbiutil.has_captcha(soup_result_page):
+    if q.startswith('http'):
+        result['result_page'] = sbiutil.search_by_url(q)
+        result['best_guess'] = sbiutil.extract_best_guess_tag(result['result_page'])
+        soup_result_page = BeautifulSoup(result['result_page'])
+        sbiutil.has_captcha(soup_result_page)
         result['titles'] = sbiutil.extract_titles(soup_result_page)
         result['snippets'] = sbiutil.extract_snippets(soup_result_page)
         result['keywords'] = sbiutil.extract_keywords(result['snippets'])
         result['best_keywords'] = sbiutil.most_common_keywords(result['best_guess'],
                                                                result['keywords'],
                                                                best_kwds_max_length)
+    else:
+        result['result_page'] = sbiutil.search_by_word(q)
+        soup_result_page = BeautifulSoup(result['result_page'])
+        if not sbiutil.has_captcha(soup_result_page):
+            sbiutil.extract_all_image_urls(soup_result_page)
     return result
--- tests/test_lib/test_google_image.py
+++ tests/test_lib/test_google_image.py
@@ -12,24 +12,29 @@ IMAGE_URL = ('http://aniapp.animate.tv/wp-content/themes/atv/images/special/'
              'madokamagica-tps-mami/chara_01.jpg')
 
 
-class test_GoogleSearchByImageUtils(object):
+
+def get_search_result(filename):
+    wdir = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(wdir, filename)
+    with open(path, 'r') as fd:
+        return fd.read()
+
+
+class test_GoogleImageSearch(object):
 
     def __init__(self):
-        self.sbiutil = google_image.GoogleSearchByImageUtils()
+        self.sbiutil = google_image.GoogleImageSearch()
 
-    @staticmethod
-    def get_search_result():
-        wdir = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(wdir, 'search_by_image.html')
-        with open(path, 'r') as fd:
-            return fd.read()
-
-    def test_search(self):
-        actual = self.sbiutil.search(IMAGE_URL)
+    def test_search_by_url(self):
+        actual = self.sbiutil.search_by_url(IMAGE_URL)
         assert_true('マミ' in actual)
 
+    def test_search_by_word(self):
+        actual = self.sbiutil.search_by_word('github')
+        assert_true('github.com' in actual)
+
     def test_extract_best_guess_tag(self):
-        result_html = self.get_search_result()
+        result_html = get_search_result('search_by_image.html')
         actual = self.sbiutil.extract_best_guess_tag(result_html)
         assert_true(any('マミ' in tag for tag in actual))
 
@@ -37,17 +42,17 @@ class test_GoogleSearchByImageUtils(object):
         html = '<input name="captcha"> </input>'
         assert_raises(Exception, self.sbiutil.has_captcha, html)
 
-        soup = BeautifulSoup(self.get_search_result())
+        soup = BeautifulSoup(get_search_result('search_by_image.html'))
         assert_false(self.sbiutil.has_captcha(soup))
 
     def test_extract_titles(self):
-        result_html = self.get_search_result()
+        result_html = get_search_result('search_by_image.html')
         soup = BeautifulSoup(result_html)
         actual = self.sbiutil.extract_titles(soup)
         assert_true(any('マミ' in snippet for snippet in actual))
 
     def test_extract_snippets(self):
-        result_html = self.get_search_result()
+        result_html = get_search_result('search_by_image.html')
         soup = BeautifulSoup(result_html)
         actual = self.sbiutil.extract_snippets(soup)
         assert_true(any('マミ' in snippet for snippet in actual))
@@ -68,10 +73,16 @@ class test_GoogleSearchByImageUtils(object):
         actual = self.sbiutil.most_common_keywords(best_guess, kwds, max_length=12)
         assert_equals(actual, ['マミさん', 'おっぱい', 'マミパイ'])
 
+    def test_extract_all_image_urls(self):
+        html = get_search_result('google_image_github.html')
+        soup_result_page = BeautifulSoup(html)
+        actual = self.sbiutil.extract_all_image_urls(soup_result_page)
+        assert_true(any('github.com' in url for url in actual))
+
 
 def test_search():
-    with mock.patch('lib.google_image.GoogleSearchByImageUtils.search') as m:
-        m.return_value = test_GoogleSearchByImageUtils.get_search_result()
-        actual = google_image.search('')
+    with mock.patch('lib.google_image.GoogleImageSearch.search_by_url') as m:
+        m.return_value = get_search_result('search_by_image.html')
+        actual = google_image.search('http://mamigazou')
         assert_true('巴マミ' in actual['best_guess'])
         assert_true('巴マミ' in actual['best_keywords'])
