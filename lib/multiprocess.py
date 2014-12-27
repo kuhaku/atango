df0da1f7b93095823db6edce56aaf09f2b61f697
kuhaku <kuhaku.mami@gmail.com>
2014-12-28 01:05:46 +0900

    :fire: Delete dead codes

--- lib/multiprocess.py
+++ lib/multiprocess.py
@@ -1,16 +1,5 @@
 from __future__ import absolute_import
-import functools
 import multiprocessing.pool
-
-
-def as_subprocess(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except KeyboardInterrupt:
-            pass
-    return wrapper
 
 
 class Pool(multiprocessing.pool.Pool):
@@ -46,19 +35,3 @@ class Pool(multiprocessing.pool.Pool):
     def imap_unordered(self, func, iterable, chunksize=1):
         return self._catch_keybord_interrupt(
             'imap_unordered', func, iterable, chunksize=chunksize)
-
-class Test:
-    def __init__(self):
-        self.y = 0
-
-    def fuga(self, x):
-        return x*x+self.y
-
-    def hoge(self):
-        p = Pool(8)
-        for i in p.imap_unordered(self.fuga, range(1000)):
-            print(i)
-
-if __name__ == "__main__":
-    test = Test()
-    test.hoge()
