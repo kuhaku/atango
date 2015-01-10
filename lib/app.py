c2e89e60477f1badf0e04556d1f91531e31648b0
kuhaku <kuhaku.mami@gmail.com>
2015-01-11 03:07:29 +0900

    上位でExceptionをトラップする

--- lib/app.py
+++ lib/app.py
@@ -75,19 +75,6 @@ class App(object):
             error_description = str(e)
             err_msg = 'TimeoutError: %s' % (error_description)
             self.logger.warn(err_msg)
-        except Exception as e:
-            error_class = e.__class__.__name__
-            error_description = str(e)
-            err_msg = '%s: %s' % (error_class, error_description)
-            self.logger.critical(err_msg)
-            tb = traceback.extract_tb(sys.exc_info()[2])
-            trace = traceback.format_list(tb)
-            self.logger.warn('---- traceback ----')
-            for line in trace:
-                text = re.sub(r'\n\s*', ' ', line.rstrip())
-                self.logger.warn(text)
-            self.logger.warn('-------------------')
-            return sys.exit(err_msg)
 
     def main(self, job):
         def sigkill_handler(self, *args):
@@ -101,3 +88,16 @@ class App(object):
             logger.warn('ぁ単語 received SIGTERM')
             twitter = api.Twitter()
             twitter.post(str(e))
+        except Exception as e:
+            error_class = e.__class__.__name__
+            error_description = str(e)
+            err_msg = '%s: %s' % (error_class, error_description)
+            self.logger.critical(err_msg)
+            tb = traceback.extract_tb(sys.exc_info()[2])
+            trace = traceback.format_list(tb)
+            self.logger.warn('---- traceback ----')
+            for line in trace:
+                text = re.sub(r'\n\s*', ' ', line.rstrip())
+                self.logger.warn(text)
+            self.logger.warn('-------------------')
+            return sys.exit(err_msg)
