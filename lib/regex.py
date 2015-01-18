6acb32e672d7db755cafc5a65392ccbd4de8d1e5
kuhaku <kuhaku.mami@gmail.com>
2015-01-18 12:15:26 +0900

    :bug: fix URL-over-matching problem

--- lib/regex.py
+++ lib/regex.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
 
-re_url = re.compile('(https?://[\w/\-@%\+\*\?\[\]\(\)#&;:!=\.,~]{1,2048})')
+re_url = re.compile('(https?://[a-zA-Z0-9_/\-@%\+\*\?\[\]\(\)#&;:!=\.,~]{3,2048})')
 re_html_tag = re.compile('<[^<]+>')
 re_a_tag = re.compile('<a[^<]+</a>', re.I)
