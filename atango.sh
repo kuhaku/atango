d393c7aea2a9a71c32d69f7cd61fb6756343917b
kuhaku <kuhaku.mami@gmail.com>
2015-02-24 02:08:26 +0900

    :bug: fix cron problem

--- atango.sh
+++ atango.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # For cron
 
 ATANGO_DIR=/work/atango
@@ -7,6 +7,7 @@ ATANGO_DIR=/work/atango
 . /work/venv/atango/bin/activate
 
 PYTHONPATH=${ATANGO_DIR}:${PYTHONPATH}
+PATH=/usr/sbin:${PATH}
 LANG="ja_JP.UTF-8"
 
 # Execute Atango Job
