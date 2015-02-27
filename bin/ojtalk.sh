c1cd6b3542b12dc870b96ad241501d4f3f7f313e
kuhaku <kuhaku.mami@gmail.com>
2015-02-27 22:50:13 +0900

    split message for avoiding openjtalk bug

--- bin/ojtalk.sh
+++ bin/ojtalk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
+# parse argument
 QUIET=false
 
 while getopts 'm:q:' OPTIONS
@@ -11,17 +12,36 @@ do
   esac
 done
 
+# initialize output file
 TMPWAVFILE=/tmp/out.wav
 if [ -e ${TMPWAVFILE} ]; then
   rm ${TMPWAVFILE}
 fi
 
-echo "${MESSAGE}" | /usr/local/bin/nkf -w | /usr/local/bin/open_jtalk \
- -m /usr/local/share/openjtalk/nitech_jp_atr503_m001.htsvoice \
- -x /usr/local/lib/mecab/dic/ojt-naist-jdic \
- -ow ${TMPWAVFILE} \
- -r 0.93
+# speech synthesis
+counter=0
+for sentence in `echo ${MESSAGE} | tr -s '' ' '`; do
+  tmpwavfile="/tmp/tmp${counter}.wav"
+  echo "${sentence}" | /usr/local/bin/nkf -w | /usr/local/bin/open_jtalk \
+   -m /usr/local/share/openjtalk/nitech_jp_atr503_m001.htsvoice \
+   -x /usr/local/lib/mecab/dic/ojt-naist-jdic \
+   -ow ${tmpwavfile} \
+   -r 0.93
+  counter=$(($counter + 1))
+done
 
+# concat wavs
+if [ $counter -gt 1 ];then
+  /usr/local/bin/ffmpeg -f concat \
+                        -i <(for f in /tmp/tmp*.wav; do echo "file '$f'"; done)\
+                        -c copy ${TMPWAVFILE} >& /dev/null
+else
+  cp ${tmpwavfile} ${TMPWAVFILE}
+fi
+
+rm /tmp/tmp*.wav
+
+# if not quiet, play
 if [ ${QUIET} != true ]; then
  afplay ${TMPWAVFILE}
 fi
