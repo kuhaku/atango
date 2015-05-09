f6074e538e1dceaeba4f90b36879ed76fd90527f
kuhaku <kuhaku.mami@gmail.com>
2015-05-10 02:00:37 +0900

    Import atango modules

--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-from .view import api, dashboard, now_or_past, sov, mecab, ma
+import sys
+sys.path.append('/work/atango/web')
+sys.path.append('/work/atango')
+from view import api, dashboard, now_or_past, sov, mecab, ma
 
 app = Flask(__name__)
 for module in (api, dashboard, now_or_past, sov, mecab, ma):
