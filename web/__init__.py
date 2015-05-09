96dd798abfd62ba8342b8dcdb79218395fd106d6
kuhaku <kuhaku.mami@gmail.com>
2015-05-10 00:36:04 +0900

    Relative import

--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-from view import api, dashboard, now_or_past, sov, mecab, ma
+from .view import api, dashboard, now_or_past, sov, mecab, ma
 
 app = Flask(__name__)
 for module in (api, dashboard, now_or_past, sov, mecab, ma):
