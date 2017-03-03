833de38721bd132db45d3a7a495f60e7e5974067
kuhaku <kuhaku.mami@gmail.com>
2017-03-04 00:38:59 +0900

    Set logger

--- web/__init__.py
+++ web/__init__.py
@@ -4,10 +4,14 @@ import sys
 sys.path.append('/work/atango/web')
 sys.path.append('/work/atango')
 from view import api, dashboard, now_or_past, sov, mecab, ma
+from lib.logger import logger
 
 app = Flask(__name__)
 for module in (api, dashboard, now_or_past, sov, mecab, ma):
     app.register_blueprint(getattr(module, 'app'))
+logger.enable_file_handler('/work/atango/logs/uwsgi_error.log')
+app.debug = True
+app.config['DEBUG'] = True
 
 
 @app.route("/")
@@ -16,3 +20,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
